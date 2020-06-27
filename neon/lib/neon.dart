e3b4c1efd0dcfd476adb231b0f3e49b220cbe8c0
Denis <gordin.dan@gmail.com>
2020-06-27 13:23:23 -0400

    add some glowing

--- neon/lib/neon.dart
+++ neon/lib/neon.dart
@@ -3,7 +3,10 @@ library neon;
 import 'package:flutter/material.dart';
 
 class Neon extends StatefulWidget {
-  Neon({Key key}) : super(key: key);
+  final String text;
+  final Color color;
+
+  Neon(this.text, this.color, {Key key}) : super(key: key);
 
   @override
   _NeonState createState() => _NeonState();
@@ -14,9 +17,62 @@ class _NeonState extends State<Neon> {
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        "Hello World",
+        widget.text,
         style: TextStyle(
-            fontFamily: Fonts.Samarin, color: Colors.purple, fontSize: 60.0),
+            fontFamily: Fonts.Beon,
+            color: Colors.blue[100],
+            fontSize: 60.0,
+            shadows: [
+              Shadow(color: Colors.blue[200], blurRadius: 10),
+              Shadow(color: Colors.blue[300], blurRadius: 20),
+              Shadow(color: Colors.blue[400], blurRadius: 30),
+            ]),
+      ),
+      // GradientText(
+      //   'Hello Flutter',
+      //   gradient: LinearGradient(colors: [
+      //     Colors.blue.shade400,
+      //     Colors.blue.shade900,
+      //     Colors.green,
+      //   ]),
+      // ),
+
+      //     RichText(
+      //   text: TextSpan(
+      //     text: widget.text,
+      //     style: DefaultTextStyle.of(context).style,
+      //     children: <TextSpan>[
+      //       TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
+      //       TextSpan(text: ' world!'),
+      //     ],
+      //   ),
+      // )
+    );
+  }
+}
+
+class GradientText extends StatelessWidget {
+  GradientText(
+    this.text, {
+    @required this.gradient,
+  });
+
+  final String text;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          // The color must be set to white for this to work
+          color: Colors.white,
+          fontSize: 40,
+        ),
       ),
     );
   }
